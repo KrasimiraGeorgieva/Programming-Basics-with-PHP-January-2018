49fe302dad8e27e428a162a2139c0f8299516dc3
Krasimira Georgieva <krassi77bg@gmail.com>
2018-01-09 01:17:46 +0200

    Move files in 2.Simple-Calculations folder.

--- Simple-Calculations/1000-days-after-birth.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$inputDate = readline();
-
-$date = new DateTime($inputDate);
-$date->modify('+999 days');
-
-echo $date->format('d-m-Y');
--- Simple-Calculations/circle-area-and-perimeter.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$r = doubleval(readline());
-
-echo $area = pi() * $r * $r , PHP_EOL;
-echo $perimeter = 2 * pi() * $r;
