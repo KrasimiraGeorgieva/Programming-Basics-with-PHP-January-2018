ac6e03e749961ece7357dc333fd8d59cfad39293
Krasimira Georgieva <krassi77bg@gmail.com>
2018-01-10 00:00:14 +0200

    Added task solution.

---
+++ 2. Simple-Calculations/08. Triangle Area.php
@@ -0,0 +1,7 @@
+<?php
+
+ $a = floatval(readline());
+ $h = floatval(readline());
+ $area = $a * $h / 2;
+ $result = round($area,2);
+ printf("Triangle area = %0.f", $result); // Triangle area = 262.500000
