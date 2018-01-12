5380c07df602657a0cc63f9f8e1144c0640a3f9c
Krasimira Georgieva <krassi77bg@gmail.com>
2018-01-12 17:32:32 +0200

    Formatting change.

--- 2. Simple-Calculations/07. 2D Rectangle Area.php
+++ 2. Simple-Calculations/07. 2D Rectangle Area.php
@@ -8,5 +8,5 @@ $y2 = readline();
 $width = abs($x1 - $x2);
 $height = abs($y1 - $y2);
 
-printf("Area = %f\n", $width * $height); //Area = 1500.000000
-printf("Perimeter = %f", 2 * ($width + $height)); //Perimeter = 160.000000
+printf("Area = %.2f\n", $width * $height); //Area = 1500.00
+printf("Perimeter = %.2f", 2 * ($width + $height)); //Perimeter = 160.00
