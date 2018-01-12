7e4139b00a46259c454370518de0a278ab50e95a
Krasimira Georgieva <krassi77bg@gmail.com>
2018-01-12 17:33:21 +0200

    Formatting change

--- 2. Simple-Calculations/08. Triangle Area.php
+++ 2. Simple-Calculations/08. Triangle Area.php
@@ -4,4 +4,4 @@
  $h = floatval(readline());
  $area = $a * $h / 2;
  $result = round($area,2);
- printf("Triangle area = %0.f", $result); // Triangle area = 262.500000
+ printf("Triangle area = %.2f", $result); // Triangle area = 262.50
