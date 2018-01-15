cca03f160eb51ef31bbd064c908136a874675ac3
Krasimira Georgieva <krassi77bg@gmail.com>
2018-01-15 23:59:46 +0200

    Added task solution.

---
+++ 3. Simple-Conditions/21.Harvest.php
@@ -0,0 +1,20 @@
+<?php
+
+$ereaGrapes = intval(readline());
+$grapesPerOneQuaterMeters = floatval(readline());
+$winesInLiters = intval(readline());
+$workers = intval(readline());
+
+$totalGrapes = $ereaGrapes * $grapesPerOneQuaterMeters;
+$totalWine = $totalGrapes * 0.40 / 2.5;
+
+if ($totalWine < $winesInLiters){
+    printf("It will be a tough winter! More %d liters wine needed.", floor($winesInLiters - $totalWine));
+}
+
+elseif ($totalWine >= $winesInLiters){
+    printf("Good harvest this year! Total wine: %d liters.", floor($totalWine));
+    echo "\n";
+    printf("%d liters left -> %d liters per person.", ceil($totalWine - $winesInLiters), ceil
+        (($totalWine - $winesInLiters) / $workers));
+}
