4ce18d01f3ea8ebdb295b790b6e7d8f9cd6577f5
Krasimira Georgieva <krassi77bg@gmail.com>
2018-02-01 15:20:05 +0200

    Added task solution.

---
+++ 5.Simple Loops/15. Smart Lili.php
@@ -0,0 +1,27 @@
+<?php
+$age = intval(readline());
+
+$priceWashingMashine = floatval(readline());
+$toyPrice = intval(readline());
+
+$money = null;
+$toys = null;
+$count = null;
+for ($i = 1; $i <= $age; $i++) {
+    if ($i % 2 == 0) {
+        $money += $i * 10 / 2;
+        $money -= 1;
+        $count++;;
+    } else {
+        $toys++;
+    }
+}
+
+$toys *= $toyPrice;
+$allMoney = $money + $toys;
+
+if ($allMoney >= $priceWashingMashine) {
+    echo "Yes! " . number_format(abs($allMoney - $priceWashingMashine), 2, '.', '');
+} else {
+    echo "No! " . number_format(abs($allMoney - $priceWashingMashine), 2, '.', '');
+}
